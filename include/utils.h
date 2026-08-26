586d87f0171b6b72538d85a0e9a33b10ccadb895
Régis LIMARE <regis.limare@opcoep.fr>
2026-08-26 17:40:26 +0200

    update version

--- include/utils.h
+++ include/utils.h
@@ -29,7 +29,7 @@
 #include <coreinit/mcp.h>
 #pragma GCC diagnostic pop
 
-#define NUSSPLI_VERSION            "1.157-ALPHA2"
+#define NUSSPLI_VERSION            "2.0"
 
 #define NAPI_URL                   "https://napi.v10lator.de/v2/"
 #define NUSSPLI_COPYRIGHT          "© 2020-2025 V10lator <v10lator@myway.de>"
